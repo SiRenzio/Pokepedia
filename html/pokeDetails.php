c73e0f1f07b52b27b441adc86eca968899b41ddc
Rence <151947583+SiRenzio@users.noreply.github.com>
2024-11-04 22:12:11 +0800

    display image

--- html/pokeDetails.php
+++ html/pokeDetails.php
@@ -10,15 +10,17 @@
         <?php
 	    	foreach($details as $title=>$pokeDetails)
 	    	{
-	    		echo  "<h1>" . "ID: " . $pokeDetails->id . "<br>";
-                echo  "Name: " . $pokeDetails->poke_name . "<br>";
-                echo  "Type1: " . $pokeDetails->type1 . "<br>";
-                echo  "Type2: " . $pokeDetails->type2 . "<br>";
-                echo  "Description: " . $pokeDetails->poke_description . "<br>";
-                echo  "Weight: " . $pokeDetails->poke_weight . "<br>";
-                echo  "Height: " . $pokeDetails->height . "<br>";
-                echo  "Mega Evolves: " . $pokeDetails->mega_evolves . "<br>";
-                echo  "Next Evolution: " . $pokeDetails->next_evolution . "<br>";
+	    		echo  "ID: " . $pokeDetails->id . "<br>";
+                echo "Name: " . $pokeDetails->poke_name . "<br>";
+                echo "Type1: " . $pokeDetails->type1 . "<br>";
+                echo "Type2: " . $pokeDetails->type2 . "<br>";
+                echo "Description: " . $pokeDetails->poke_description . "<br>";
+                echo "Weight: " . $pokeDetails->poke_weight . "<br>";
+                echo "Height: " . $pokeDetails->height . "<br>";
+                echo "Mega Evolves: " . $pokeDetails->mega_evolves . "<br>";
+                echo "Next Evolution: " . $pokeDetails->next_evolution . "<br>";
+                echo "<img src='" . $pokeDetails->poke_image . "' alt='poke_img' class='poke_img'>" . "<br>";
+
 	    	}
 	    ?>
     </div>
